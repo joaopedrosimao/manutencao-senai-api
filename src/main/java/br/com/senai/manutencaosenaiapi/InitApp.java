5a558f5a1c2f629d2f11ff260b150dfba53d59ce
Joao Pedro Simao <joaopedrodasilvasimao@gmail.com>
2022-04-14 21:44:26 -0300

    Criação da entidade ordem de serviço. Criação da camada de service

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -2,6 +2,8 @@ package br.com.senai.manutencaosenaiapi;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,10 +13,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import br.com.senai.manutencaosenaiapi.entity.Cliente;
+import br.com.senai.manutencaosenaiapi.entity.OrdemDeServico;
 import br.com.senai.manutencaosenaiapi.entity.Peca;
 import br.com.senai.manutencaosenaiapi.entity.Tecnico;
-import br.com.senai.manutencaosenaiapi.enums.Sexo;
 import br.com.senai.manutencaosenaiapi.service.ClienteService;
+import br.com.senai.manutencaosenaiapi.service.OrdemDeServicoService;
+import br.com.senai.manutencaosenaiapi.service.PecaService;
 import br.com.senai.manutencaosenaiapi.service.TecnicoService;
 
 @SpringBootApplication
@@ -29,11 +33,32 @@ public class InitApp {
 	
 	@Autowired
 	private ClienteService clienteService;
-
+	
+	@Autowired
+	private PecaService pecaService;
+	
+	@Autowired
+	private OrdemDeServicoService ordemService;
+	
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ac) {
 		return args -> {
 			try {
+				OrdemDeServico novaOrdem = new OrdemDeServico();
+				novaOrdem.setDescricaoDoProblema("Problema");
+				Cliente cliente = new Cliente();
+				cliente.setId(1);
+				novaOrdem.setCliente(cliente);
+				novaOrdem.setDataDeAbertura(LocalDate.now());
+				Tecnico tecnico = new Tecnico();
+				tecnico.setId(1);
+				novaOrdem.setTecnico(tecnico);
+				Peca peca = new Peca();
+				//peca.setId(1);
+				List<Peca> pecas = new ArrayList<Peca>();
+				pecas.add(peca);
+				novaOrdem.setPecasDoReparo(pecas);
+				this.ordemService.inserir(novaOrdem);
 				/*Tecnico novoTecnico = new Tecnico();
 				novoTecnico.setNomeCompleto("Josevildo Soares");
 				LocalDate dataDeAdmissao = LocalDate.of(2022, 4, 7);
